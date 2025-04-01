dfe25494c7f74c3e5450f5648eb8da37f2a6f8d0
Gayathri S <gayathrideveloper03@gmail.com>
2025-04-01 10:57:14 +0530

    Update April-1-passing multiple props.js

--- April-1-passing multiple props.js
+++ April-1-passing multiple props.js
@@ -1,1 +1,56 @@
+//App.js
+
+import logo from './logo.svg';
+import './App.css';
+import Greet from './Components/Greet';
+import Welcome from './Components/Welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet name="Gayathri" age={22}/>  //If you write age="25", React treats "25" as a string, not a number.Using {25} ensures React treats it as a JavaScript number, allowing calculations like age + 5.
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
+
+
+//Greet.js
+
+import React from 'react'
+
+function Greet(props){
+    return(
+        <div>
+            <h1>Welcome {props.name}</h1>
+            <h1>Age: {props.age}</h1>
+        </div>
+    )
+}
+
+
+export default Greet;
+
+
+// Different way of doing it. you can use React Fragments (<>...</>) to avoid unnecessary HTML elements.
+
+
+import React from 'react'
+
+function Greet(props){
+    return(
+        <>
+            <h1>Welcome {props.name}</h1>
+            <h1>Age: {props.age}</h1>
+        </>
+    )
+}
+
+
+export default Greet;
 
