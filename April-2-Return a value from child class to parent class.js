d353cdc938afc299cd53bc65c891ee7d9270052c
Gayathri S <gayathrideveloper03@gmail.com>
2025-04-02 17:58:25 +0530

    Create April-2-Return a value from child class to parent class.js

---
+++ April-2-Return a value from child class to parent class.js
@@ -0,0 +1,66 @@
+//App.js
+
+import logo from './logo.svg';
+import './App.css';
+import EventBind from './Components/EventBind';
+import ParentComponent from './Components/ParentComponent';
+function App() {
+  return (
+    <div className="App">
+      <ParentComponent></ParentComponent>
+    </div>
+  );
+}
+
+export default App;
+
+//ParentComponent.js
+
+import React, { Component } from 'react'
+import ChildComponent from './ChildComponent'
+
+ class ParentComponent extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         parentName:"Parent"
+      }
+      this.greetParent=this.greetParent.bind(this)
+    }
+    greetParent(C)
+    {
+        alert(`Hello ${this.state.parentName} from ${C}`)
+    }
+    
+  render() {
+    return (
+      <div>
+        <ChildComponent greetHandler={this.greetParent}> </ChildComponent>
+      </div>
+    )
+  }
+}
+
+export default ParentComponent
+
+
+
+//ChildComponent.js
+
+import React from 'react'
+
+function ChildComponent(props) {
+  return (
+    <div>
+   
+      
+
+      <button onClick={() =>props.greetHandler("Child")}>greetParent</button>
+    </div>
+  )
+}
+
+export default ChildComponent
+
+
