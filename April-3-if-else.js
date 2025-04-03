db08952c7d6ab72f5b1d33fa23ce53c23d3fdcd0
Gayathri S <gayathrideveloper03@gmail.com>
2025-04-03 14:44:52 +0530

    Create April-3-if-else.js

---
+++ April-3-if-else.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react"
+
+class UserGreeting extends Component{
+    constructor(props)
+    {
+        super(props)
+        this.state ={
+            isLoggedIn :true
+        }
+    }
+
+    render(){
+
+        if(this.state.isLoggedIn)
+        {
+            return(
+                <div>
+                    Welcome Gayathri
+                </div>
+            )
+        }
+        else{
+            return(
+                <div>
+                    Welcome Guest
+                </div>
+            )
+        }
+       
+        
+    }
+}
+export default UserGreeting
