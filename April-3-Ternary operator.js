268d51c0f23a75d4fbc7155ebfa4d819038188c5
Gayathri S <gayathrideveloper03@gmail.com>
2025-04-03 15:06:53 +0530

    Create April-3-Ternary operator.js

---
+++ April-3-Ternary operator.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react"
+
+class UserGreeting extends Component{
+    constructor(props)
+    {
+        super(props)
+        this.state ={
+            isLoggedIn :true
+        }
+    }
+
+   render(){
+    return(
+        this.state.isLoggedIn ? <div>Welcome Gayathri</div> : <div>Welcome Guest</div>
+    )
+   }
+}
+export default UserGreeting
