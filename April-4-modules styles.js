9032eb93b15c1563c6e0cebafea185fff1eb8aa9
Gayathri S <gayathrideveloper03@gmail.com>
2025-04-04 13:32:06 +0530

    Create April-4-modules styles.js

---
+++ April-4-modules styles.js
@@ -0,0 +1,34 @@
+//App.js
+
+import logo from './logo.svg';
+import './App.css';
+import './appStyle.css'
+import styles from './appStyle.module.css'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+   
+    </div>
+  );
+}
+
+export default App;
+
+//appStyle.css
+
+.error{
+    color: red;
+}
+
+
+//appStyle.module.css
+
+.error{
+    color:greenyellow;
+}
