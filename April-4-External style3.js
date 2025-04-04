86cdc081184f644e22964cae4ee69248cdf205eb
Gayathri S <gayathrideveloper03@gmail.com>
2025-04-04 12:44:04 +0530

    Create April-4-External style3.js

---
+++ April-4-External style3.js
@@ -0,0 +1,42 @@
+//App.js
+
+import logo from './logo.svg';
+import './App.css';
+import Stylesheet from './Components/Stylesheet';
+
+
+function App() {
+  return (
+    <div className="App">
+   <Stylesheet primary={true}></Stylesheet>
+    </div>
+  );
+}
+
+export default App;
+
+
+//Stylesheet.js
+
+import React from 'react'
+import './myStyle.css'
+
+function Stylesheet(props){
+    let className=props.primary ? 'primary' :'';
+    return(
+        
+        <div>
+            <h1 className={`${className} font-xl`}>Stylesheet</h1> 
+        </div>
+    )
+}
+export default Stylesheet
+
+//myStyle.css
+.primary{
+    color: rebeccapurple;
+}
+
+.font-xl{
+    font-size:72px;
+}
